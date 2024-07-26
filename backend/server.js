7b4c5cf08f83820185cc969a79a3fbcf2efeed3d
Rupesh Pancha <rupeshpancha7.com>
2024-07-26 15:18:49 +0000

    prjoect setup

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from "express"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+app.get("/",(req,res) => {
+    res.send ("hellow world")
+})
+
+app.listen(PORT,()=>{
+    console.log("server is running on port " + PORT)
+})
+
+
