976358a6885d296f42e1e95eddd2d92b4320348d
Rupesh Pancha <rupeshpancha7.com>
2024-07-27 08:53:07 +0000

    database connect

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,27 @@
 import express from "express"
 import dotenv from "dotenv"
+import mongoose from "mongoose"
 
 dotenv.config()
+
+console.log('MONGO_URI:', process.env.MONGO_URI);
+
+mongoose
+    .connect(process.env.MONGO_URI).then(() => {
+        console.log("connected to mangodb")
+    }).catch((err) => {
+        console.log(err)
+    })
 
 const app = express()
 
 const PORT = process.env.PORT || 3000
 
-app.get("/",(req,res) => {
-    res.send ("hellow world")
+app.get("/", (req, res) => {
+    res.send("hellow world")
 })
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log("server is running on port " + PORT)
 })
 
